5aa1b0a932f6e7062d1d9f7d315a7efe0f743249
baoziii <baoziii@126.com>
2021-05-09 13:04:17 -0400

    Add Bilibili Support & Config file comment Chinese translation

--- Chat/ChatConfig.cs
+++ Chat/ChatConfig.cs
@@ -34,47 +34,47 @@ namespace EnhancedStreamChat.Chat
         public static ChatConfig instance { get; private set; } = new ChatConfig(Path.Combine(Environment.CurrentDirectory, "UserData"), Assembly.GetExecutingAssembly().GetName().Name);
 
         [ConfigSection("Main")]
-        [ConfigMeta(Comment = "When set to true, animated emotes will be precached in memory when the game starts.")]
+        [ConfigMeta(Comment = "当改项为true时，动画表情将会在游戏启动时预载到内存 When set to true, animated emotes will be precached in memory when the game starts.")]
         public bool PreCacheAnimatedEmotes = true;
 
         [ConfigSection("UI")]
-        [ConfigMeta(Comment = "The name of the system font to be used in chat")]
+        [ConfigMeta(Comment = "聊天框中使用的系统字体名称 The name of the system font to be used in chat")]
         public string SystemFontName = "Segoe UI";
-        [ConfigMeta(Comment = "The background color of the chat")]
+        [ConfigMeta(Comment = "聊天框中的背景颜色 The background color of the chat")]
         public Color BackgroundColor = Color.black.ColorWithAlpha(0.87f);
-        [ConfigMeta(Comment = "The base color of the chat text.")]
+        [ConfigMeta(Comment = "聊天框中的文字颜色 The base color of the chat text.")]
         public Color TextColor = Color.white;
-        [ConfigMeta(Comment = "The accent color to be used on system messages")]
+        [ConfigMeta(Comment = "聊天框中的着重色 The accent color to be used on system messages")]
         public Color AccentColor = new Color(0.57f, 0.28f, 1f, 1f);
-        [ConfigMeta(Comment = "The highlight color to be used on system messages")]
+        [ConfigMeta(Comment = "聊天框中的高亮色 The highlight color to be used on system messages")]
         public Color HighlightColor = new Color(0.57f, 0.28f, 1f, 0.06f);
-        [ConfigMeta(Comment = "The color pings will be highlighted as in chat")]
+        [ConfigMeta(Comment = "聊天框中的系统消息色 The color pings will be highlighted as in chat")]
         public Color PingColor = new Color(1f, 0f, 0f, 0.13f);
 
         [ConfigSection("General Layout")]
-        [ConfigMeta(Comment = "The width of the chat")]
+        [ConfigMeta(Comment = "聊天框宽度 The width of the chat")]
         public int ChatWidth = 120;
-        [ConfigMeta(Comment = "The height of the chat")]
+        [ConfigMeta(Comment = "聊天框高度 The height of the chat")]
         public int ChatHeight = 140;
-        [ConfigMeta(Comment = "The size of the font")]
+        [ConfigMeta(Comment = "字体大小 The size of the font")]
         public float FontSize = 3.4f;
-        [ConfigMeta(Comment = "Allow movement of the chat")]
+        [ConfigMeta(Comment = "允许移动聊天框 Allow movement of the chat")]
         public bool AllowMovement = false;
-        [ConfigMeta(Comment = "Sync positions and rotations for the chat in menu and in-game")]
+        [ConfigMeta(Comment = "游戏/菜单中聊天框显示在相同位置 Sync positions and rotations for the chat in menu and in-game")]
         public bool SyncOrientation = false;
-        [ConfigMeta(Comment = "Reverse the order of the chat")]
+        [ConfigMeta(Comment = "从上往下显示消息 Reverse the order of the chat")]
         public bool ReverseChatOrder = false;
 
         [ConfigSection("In-Menu Layout")]
-        [ConfigMeta(Comment = "The world position of the chat while at the main menu")]
+        [ConfigMeta(Comment = "菜单中聊天框的世界坐标轴坐标 The world position of the chat while at the main menu")]
         public Vector3 Menu_ChatPosition = new Vector3(0, 3.75f, 2.5f);
-        [ConfigMeta(Comment = "The world rotation of the chat while at the main menu")]
+        [ConfigMeta(Comment = "菜单中聊天框的世界坐标轴旋转 The world rotation of the chat while at the main menu")]
         public Vector3 Menu_ChatRotation = new Vector3(325, 0, 0);
 
         [ConfigSection("In-Song Layout")]
-        [ConfigMeta(Comment = "The world position of the chat while in-song")]
+        [ConfigMeta(Comment = "游戏中聊天框的世界坐标轴坐标 The world position of the chat while in-song")]
         public Vector3 Song_ChatPosition = new Vector3(0, 3.75f, 2.5f);
-        [ConfigMeta(Comment = "The world rotation of the chat while in-song")]
+        [ConfigMeta(Comment = "游戏中聊天框的世界坐标轴旋转 The world rotation of the chat while in-song")]
         public Vector3 Song_ChatRotation = new Vector3(325, 0, 0);
 
 
--- Chat/ChatMessageBuilder.cs
+++ Chat/ChatMessageBuilder.cs
@@ -88,50 +88,62 @@ namespace EnhancedStreamChat.Chat
         public static Task<string> BuildMessage(IChatMessage msg, EnhancedFontInfo font, bool isInGame = false) => Task.Run(() =>
                                                                                                                              {
                                                                                                                                  try {
-                                                                                                                                     if (!PrepareImages(msg, font)) {
-                                                                                                                                         Logger.Warn($"Failed to prepare some/all images for msg \"{msg.Message}\"!");
-                                                                                                                                         //return msg.Message;
-                                                                                                                                     }
-
-                                                                                                                                     var badges = new ConcurrentStack<EnhancedImageInfo>();
-                                                                                                                                     foreach (var badge in msg.Sender.Badges) {
-                                                                                                                                         if (!ChatImageProvider.instance.CachedImageInfo.TryGetValue(badge.Id, out var badgeInfo)) {
-                                                                                                                                             Logger.Warn($"Failed to find cached image info for badge \"{badge.Id}\"!");
-                                                                                                                                             continue;
-                                                                                                                                         }
-                                                                                                                                         badges.Push(badgeInfo);
-                                                                                                                                     }
-
                                                                                                                                      var sb = new StringBuilder(msg.Message); // Replace all instances of < with a zero-width non-breaking character
-
-                                                                                                                                     // Escape all html tags in the message
-                                                                                                                                     sb.Replace("<", "<\u2060");
-
-                                                                                                                                     foreach (var emote in msg.Emotes) {
-                                                                                                                                         if (!ChatImageProvider.instance.CachedImageInfo.TryGetValue(emote.Id, out var replace)) {
-                                                                                                                                             Logger.Warn($"Emote {emote.Name} was missing from the emote dict! The request to {emote.Uri} may have timed out?");
-                                                                                                                                             continue;
+                                                                                                                                     var badges = new ConcurrentStack<EnhancedImageInfo>();
+                                                                                                                                     if (msg.Emotes.Length == 0)
+                                                                                                                                     {
+                                                                                                                                         sb = new StringBuilder(msg.Message); // Replace all instances of < with a zero-width non-breaking character
+                                                                                                                                     }
+                                                                                                                                     else {
+                                                                                                                                         if (!PrepareImages(msg, font))
+                                                                                                                                         {
+                                                                                                                                             Logger.Warn($"Failed to prepare some/all images for msg \"{msg.Message}\"!");
+                                                                                                                                             //return msg.Message;
                                                                                                                                          }
-                                                                                                                                         //Logger.Info($"Emote: {emote.Name}, StartIndex: {emote.StartIndex}, EndIndex: {emote.EndIndex}, Len: {sb.Length}");
-                                                                                                                                         if (!font.TryGetCharacter(replace.ImageId, out var character)) {
-                                                                                                                                             Logger.Warn($"Emote {emote.Name} was missing from the character dict! Font hay have run out of usable characters.");
-                                                                                                                                             continue;
-                                                                                                                                         }
-
-                                                                                                                                         try {
-                                                                                                                                             // Replace emotes by index, in reverse order (msg.Emotes is sorted by emote.StartIndex in descending order)
-                                                                                                                                             sb.Replace(emote.Name, emote switch
+                                                                                                                                         badges = new ConcurrentStack<EnhancedImageInfo>();
+                                                                                                                                         foreach (var badge in msg.Sender.Badges)
+                                                                                                                                         {
+                                                                                                                                             if (!ChatImageProvider.instance.CachedImageInfo.TryGetValue(badge.Id, out var badgeInfo))
                                                                                                                                              {
-                                                                                                                                                 TwitchEmote t when t.Bits > 0 => $"{char.ConvertFromUtf32((int)character)}\u00A0<color={t.Color}><size=77%><b>{t.Bits}\u00A0</b></size></color>",
-                                                                                                                                                 _ => char.ConvertFromUtf32((int)character)
-                                                                                                                                             },
-                                                                                                                                                                                                                                                              emote.StartIndex, emote.EndIndex - emote.StartIndex + 1);
+                                                                                                                                                 Logger.Warn($"Failed to find cached image info for badge \"{badge.Id}\"!");
+                                                                                                                                                 continue;
+                                                                                                                                             }
+                                                                                                                                             badges.Push(badgeInfo);
                                                                                                                                          }
-                                                                                                                                         catch (Exception ex) {
-                                                                                                                                             Logger.Error($"An unknown error occurred while trying to swap emote {emote.Name} into string of length {sb.Length} at location ({emote.StartIndex}, {emote.EndIndex})\r\n{ex}");
+                                                                                                                                         sb = new StringBuilder(msg.Message); // Replace all instances of < with a zero-width non-breaking character
+
+                                                                                                                                         // Escape all html tags in the message
+                                                                                                                                         sb.Replace("<", "<\u2060");
+                                                                                                                                         foreach (var emote in msg.Emotes)
+                                                                                                                                         {
+                                                                                                                                             if (!ChatImageProvider.instance.CachedImageInfo.TryGetValue(emote.Id, out var replace))
+                                                                                                                                             {
+                                                                                                                                                 Logger.Warn($"Emote {emote.Name} was missing from the emote dict! The request to {emote.Uri} may have timed out?");
+                                                                                                                                                 continue;
+                                                                                                                                             }
+                                                                                                                                             //Logger.Info($"Emote: {emote.Name}, StartIndex: {emote.StartIndex}, EndIndex: {emote.EndIndex}, Len: {sb.Length}");
+                                                                                                                                             if (!font.TryGetCharacter(replace.ImageId, out var character))
+                                                                                                                                             {
+                                                                                                                                                 Logger.Warn($"Emote {emote.Name} was missing from the character dict! Font hay have run out of usable characters.");
+                                                                                                                                                 continue;
+                                                                                                                                             }
+                                                                                                                                             try
+                                                                                                                                             {
+                                                                                                                                                 // Replace emotes by index, in reverse order (msg.Emotes is sorted by emote.StartIndex in descending order)
+                                                                                                                                                 sb.Replace(emote.Name, emote switch
+                                                                                                                                                 {
+                                                                                                                                                     TwitchEmote t when t.Bits > 0 => $"{char.ConvertFromUtf32((int)character)}\u00A0<color={t.Color}><size=77%><b>{t.Bits}\u00A0</b></size></color>",
+                                                                                                                                                     _ => char.ConvertFromUtf32((int)character)
+                                                                                                                                                 },
+                                                                                                                                                                                                                                                                  emote.StartIndex, emote.EndIndex - emote.StartIndex + 1);
+                                                                                                                                             }
+                                                                                                                                             catch (Exception ex)
+                                                                                                                                             {
+                                                                                                                                                 Logger.Error($"An unknown error occurred while trying to swap emote {emote.Name} into string of length {sb.Length} at location ({emote.StartIndex}, {emote.EndIndex})\r\n{ex}");
+                                                                                                                                             }
                                                                                                                                          }
                                                                                                                                      }
-
+                                                                                                                                     
                                                                                                                                      if (msg.IsSystemMessage) {
                                                                                                                                          // System messages get a grayish color to differenciate them from normal messages in chat, and do not receive a username/badge prefix
                                                                                                                                          sb.Insert(0, $"<color=#bbbbbbbb>");
@@ -147,7 +159,6 @@ namespace EnhancedStreamChat.Chat
                                                                                                                                              // Insert username w/ color
                                                                                                                                              sb.Insert(0, $"<color={msg.Sender.Color}><b>{msg.Sender.DisplayName}</b></color>: ");
                                                                                                                                          }
-
                                                                                                                                          for (var i = 0; i < msg.Sender.Badges.Length; i++) {
                                                                                                                                              // Insert user badges at the beginning of the string in reverse order
                                                                                                                                              if (badges.TryPop(out var badge) && font.TryGetCharacter(badge.ImageId, out var character)) {
